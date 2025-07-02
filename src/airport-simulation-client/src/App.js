c88dec399176a5443c5415fbe3631952d703da50
ovidiuvintu <ovidiuvintu@outlook.com>
2025-07-02 15:54:31 -0500

    Gui initial setup development

--- src/airport-simulation-client/src/App.js
+++ src/airport-simulation-client/src/App.js
@@ -1,8 +1,20 @@
 
+import Topbar from "./components/Topbar";
+import Sidebar from "./components/Sidebar";
+
+
 function App() {
   return (
     <div className="app">
-
+      <Topbar />
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+      <main className="content">
+        {/* Main content goes here */}
+        <h1>Welcome to the Airport Simulation</h1>
+        <p>This is the main content area.</p>
+      </main>
     </div>
   );
 }
---
+++ src/airport-simulation-client/src/theme.js
@@ -0,0 +1,9 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+// Color design tokens
+#666666
+#141bd2 //primary color
+#4cceac
+#db4f4a
+#6870fa
