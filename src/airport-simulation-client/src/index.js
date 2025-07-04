0c26e1f36389db47fc5f5acfd2d95f20322ae4c4
ovidiuvintu <ovidiuvintu@outlook.com>
2025-07-04 08:40:23 -0500

    Initial revision

---
+++ src/airport-simulation-client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
