bd29839ed7dcb9a5a8b61c03ff53a58442d3529d
ovidiuvintu <ovidiuvintu@outlook.com>
2025-07-06 20:14:45 -0500

    Added sidebar menu items

--- src/airport-simulation-client/src/App.js
+++ src/airport-simulation-client/src/App.js
@@ -1,16 +1,29 @@
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Arrivals from "./scenes/arrivals";
+// import Departures from "./scenes/departures";
+// import Airlines from "./scenes/airlines";
+
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from 'react-router-dom'
+import { useState } from "react";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar isSidebar={isSidebar} /> 
           <main className="content">
-            <Topbar />
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
---
+++ src/airport-simulation-client/src/scenes/dashboard/index.jsx
@@ -0,0 +1,26 @@
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
+const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+      </Box>
+
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
