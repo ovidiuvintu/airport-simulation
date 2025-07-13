8ccbffa407e386baccb707bcce32e6ab0b0637ca
ovidiuvintu <ovidiuvintu@outlook.com>
2025-07-13 12:06:42 -0500

    Changed the schedule service to transient

--- src/Airport.Simulation/Airport.Scheduler/Program.cs
+++ src/Airport.Simulation/Airport.Scheduler/Program.cs
@@ -9,7 +9,7 @@ builder.AddServiceDefaults();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSingleton<IScheduleServices, ScheduleServices>();
+builder.Services.AddTransient<IScheduleServices, ScheduleServices>();
 
 var app = builder.Build();
 
