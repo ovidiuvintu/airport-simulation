fab4193171c0738cfbe8ec23ffbfb50adf60f869
ovidiuvintu <ovidiuvintu@outlook.com>
2025-08-02 17:05:20 -0500

    Added initial components

--- src/AirportSimulation/airport-simulation-client/src/App.js
+++ src/AirportSimulation/airport-simulation-client/src/App.js
@@ -1,15 +1,36 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./scenes/dashboard/Dashboard";
+import Arrivals from "./scenes/arrivals/Arrivals";
+import Departures from "./scenes/departures/Departures";
+import Airlines from "./scenes/airlines/Airlines";
+import Airports from "./scenes/airports/Airports";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="app">
-          </div>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/arrivals" element={<Arrivals />} />
+              <Route path="/departures" element={<Departures />} />
+              <Route path="/airlines" element={<Airlines />} />
+              <Route path="/airports" element={<Airports />} />
+            </Routes>
+          </main>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
---
+++ src/AirportSimulation/airport-simulation-client/src/scenes/dashboard/Dashboard.jsx
@@ -0,0 +1,19 @@
+import { Box, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+
+const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>      
+      {/* ICONS */}
+      <Box display="flex">       
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
