243ad253023b48e309efc70826e2cd7149319e22
ovidiuvintu <ovidiuvintu@outlook.com>
2025-08-30 08:00:14 -0500

    More ui works

--- src/AirportSimulation/airport-simulation-client/src/scenes/dashboard/Dashboard.jsx
+++ src/AirportSimulation/airport-simulation-client/src/scenes/dashboard/Dashboard.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import { Typography, Box, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
 
 
 import AppToolbar from "../../components/AppToolbar";
@@ -13,28 +14,31 @@ import AppToolbar from "../../components/AppToolbar";
 import '../../index.css';
 
 const TwoBoxesSideBySide = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
     return (
         <Box sx={{ display: 'flex', gap: '16px', my: '10px' }}> {/* Parent Box with flex display and gap */}
-            <Box sx={{ flex: 1, bgcolor: 'primary.main', display: 'flex', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
+            <Box sx={{ flex: 1, bgcolor: colors.primary[400], display: 'flex', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
                 <section>
                     <div class="runway" />
                     <img src="plane.png" class="plane"/>
                 </section>
             </Box>
-            <Box sx={{ flex: 3, bgcolor: 'primary.main', display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
-                <Stack sx={{ spacing: '1', bgcolor: 'primary.main' }} >
-                    <Typography display="block" variant="h4">Flight Details</Typography>
-                    <Typography display="block" variant="h6">Flight Number:</Typography>
-                    <Typography display="block" variant="h6">Time:</Typography>
-                    <Typography display="block" variant="h6">Origin/Destination:</Typography>
+            <Box sx={{ flex: 3, bgcolor: colors.primary[400], display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
+                <Stack sx={{ spacing: '1', bgcolor: colors.primary[400] }} >
+                    <Typography sx={{ display: "block", variant: "h4", color: colors.greenAccent[100] }} >Flight Details</Typography>
+                    <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Flight Number:</Typography>
+                    <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Time:</Typography>
+                    <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Origin/Destination:</Typography>
                 </Stack>
             </Box>
-            <Box sx={{ flex: 3, bgcolor: 'primary.main', display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
-                <Stack sx={{ spacing: '1', bgcolor: 'primary.main' }} >
-                    <Typography display="block" variant="h4">Next Flight Details</Typography>
-                    <Typography display="block" variant="h6">Flight Number:</Typography>
-                    <Typography display="block" variant="h6">Time:</Typography>
-                    <Typography display="block" variant="h6">Origin/Destination:</Typography>
+            <Box sx={{ flex: 3, bgcolor: colors.primary[400], display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
+                <Stack sx={{ spacing: '1', bgcolor: colors.primary[400] }} >
+                    <Typography sx={{ display: "block", variant: "h4", color: colors.greenAccent[100] }}>Next Flight Details</Typography>
+                    <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Flight Number:</Typography>
+                    <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Time:</Typography>
+                    <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Origin/Destination:</Typography>
                 </Stack>
             </Box>
         </Box>
@@ -42,6 +46,7 @@ const TwoBoxesSideBySide = () => {
 }
 
 const Dashboard = () => {
+  
   return (
     <Box m="20px">
           <AppToolbar />   
