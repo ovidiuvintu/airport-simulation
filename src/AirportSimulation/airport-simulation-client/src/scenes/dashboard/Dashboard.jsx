0dfb276e64651bd566b572b20d98780867447258
ovidiuvintu <ovidiuvintu@outlook.com>
2025-09-13 13:03:50 -0500

    Fixed the containers width and height

--- src/AirportSimulation/airport-simulation-client/src/scenes/dashboard/Dashboard.jsx
+++ src/AirportSimulation/airport-simulation-client/src/scenes/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
-import { Typography, Box, useTheme } from "@mui/material";
+import { Typography, Box, Grid, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import { useRunwayVisibilityStore } from '../../store/useStore';
 
@@ -17,33 +17,41 @@ const TwoBoxesSideBySide = () => {
 
     return (
             <Box sx={{ height:"65vh", display: 'flex', gap: '16px', my: '10px' }}> {/* Parent Box with flex display and gap */}
-                <Box sx={{flex: 3, bgcolor: colors.primary[400], display: 'flex', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
-                    {/* {isVisible?
-                        <section>
-                            <div class="runway" />
-                            <img src="plane.png" class="plane"/>
-                        </section>:
+                <Grid container spacing={2}> {/* Grid container */}
+                    <Grid item xs={12} sm={3}> {/* Child Box 1: 1/3 width on small screens and up */}
+                        <Box sx={{ p: 2, bgcolor: 'primary.main', color: 'white' }}>
+                            {/* {isVisible?
+                                <section>
+                                    <div class="runway" />
+                                    <img src="plane.png" class="plane"/>
+                                </section>:
 
-                        <section>
-                            <div class="runwaypaused" />
-                        </section>} */}
-                </Box>
-                <Box sx={{ flex: 3, bgcolor: colors.primary[400], display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
-                    {/* <Stack sx={{ spacing: '1', bgcolor: colors.primary[400] }} >
-                        <Typography sx={{ display: "block", variant: "h4", color: colors.greenAccent[100] }} >Flight Details</Typography>
-                        <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Flight Number:</Typography>
-                        <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Time:</Typography>
-                        <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Origin/Destination:</Typography>
-                    </Stack> */}
-                </Box>
-                <Box sx={{ flex: 3, bgcolor: colors.primary[400], display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', color: 'white' }}>
-                    {/* <Stack sx={{ spacing: '1', bgcolor: colors.primary[400] }} >
-                        <Typography sx={{ display: "block", variant: "h4", color: colors.greenAccent[100] }}>Next Flight Details</Typography>
-                        <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Flight Number:</Typography>
-                        <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Time:</Typography>
-                        <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Origin/Destination:</Typography>
-                    </Stack> */}
-                </Box>
+                                <section>
+                                    <div class="runwaypaused" />
+                                </section>} */}
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={4.5}> {/* Child Box 2: 1/2 width on small screens and up */}
+                        <Box sx={{ p: 2 }}>
+                            <Stack sx={{ spacing: '1', bgcolor: colors.primary[400] }} >
+                                <Typography sx={{ display: "block", variant: "h4", color: colors.greenAccent[100] }} >Flight Details</Typography>
+                                <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Flight Number:</Typography>
+                                <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Time:</Typography>
+                                <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Origin/Destination:</Typography>
+                            </Stack>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={4.5}> {/* Child Box 3: 1/6 width on small screens and up */}
+                        <Box sx={{ p: 2 }}>
+                            <Stack sx={{ spacing: '1', bgcolor: colors.primary[400] }} >
+                                <Typography sx={{ display: "block", variant: "h4", color: colors.greenAccent[100] }}>Next Flight Details</Typography>
+                                <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Flight Number:</Typography>
+                                <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Time:</Typography>
+                                <Typography sx={{ display: "block", variant: "h6", color: colors.greenAccent[100] }}>Origin/Destination:</Typography>
+                            </Stack>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Box>
     );
 }
